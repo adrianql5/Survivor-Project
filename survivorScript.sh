986dece1822c6e46ab16459c6d54c6fc26c993cf
adrianql <you@example.com>
2025-10-27 13:58:20 +0100

    Actualización automática. Archivos modificados: survivorScript.sh

--- survivorScript.sh
+++ survivorScript.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright (c) 2025 Adrián Quiroga Linares Lectura y referencia permitidas; reutilización y plagio prohibidos
+
 
 curl -fsSL https://ollama.com/install.sh | sh
 sleep 5
